11e3f3eb0716c5252e7002f32f7d53f7112842c1
Bruno Van de Velde <bruno@texus.me>
2017-03-19 17:58:01 +0100

    Squashed 'LoRaMac-node/' changes from bccf111f..ff0ade97
    ff0ade97 Updated readme on how to build a different app
    9c10378a Derive NAME from TARGET
    7ff5fb81 Makefile can now build any of the apps
    83f369a2 Added SX1276SetRfTxPower that was added upstream
    
    git-subtree-dir: LoRaMac-node
    git-subtree-split: ff0ade970ca6e7946a92b39ddf5027c7f4b1f6f8

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 This project is a port of the [Lora-net LoRaWAN stack implementation](https://github.com/Lora-net/LoRaMac-node) to work on the EFM32 Giant Gecko (STK3700) hardware with a HopeRF RFM95W radio (which is compatible with the SX1276).
 
-Note that some parts were not ported (e.g. I2C and GPS), only the code required to use the radio is implemented.
+Note that some parts were not ported (e.g. I2C and GPS), only the code required to use the radio and the uart are implemented.
 
-The code and the example from `src/apps/LoRaMac/classA/EFM32GG_STK3700` can be build by simply running `make` in the root directory.
+To compile one of the apps, update `Makefile.local` (or set the `TARGET` environment variable) and run `make` from the root directory. To compile e.g. the class A end-device code from `src/apps/LoRaMac/classA/EFM32GG_STK3700`, the `TARGET` variable should be `LoRaMac/classA`.
--- src/boards/EFM32GG_STK3700/sx1276-board.c
+++ src/boards/EFM32GG_STK3700/sx1276-board.c
@@ -83,6 +83,68 @@ void SX1276IoDeInit( void )
 	assert_param( FAIL );
 }
 
+void SX1276SetRfTxPower( int8_t power )
+{
+    uint8_t paConfig = 0;
+    uint8_t paDac = 0;
+
+    paConfig = SX1276Read( REG_PACONFIG );
+    paDac = SX1276Read( REG_PADAC );
+
+    paConfig = ( paConfig & RF_PACONFIG_PASELECT_MASK ) | SX1276GetPaSelect( SX1276.Settings.Channel );
+    paConfig = ( paConfig & RF_PACONFIG_MAX_POWER_MASK ) | 0x70;
+
+    if( ( paConfig & RF_PACONFIG_PASELECT_PABOOST ) == RF_PACONFIG_PASELECT_PABOOST )
+    {
+        if( power > 17 )
+        {
+            paDac = ( paDac & RF_PADAC_20DBM_MASK ) | RF_PADAC_20DBM_ON;
+        }
+        else
+        {
+            paDac = ( paDac & RF_PADAC_20DBM_MASK ) | RF_PADAC_20DBM_OFF;
+        }
+        if( ( paDac & RF_PADAC_20DBM_ON ) == RF_PADAC_20DBM_ON )
+        {
+            if( power < 5 )
+            {
+                power = 5;
+            }
+            if( power > 20 )
+            {
+                power = 20;
+            }
+            paConfig = ( paConfig & RF_PACONFIG_OUTPUTPOWER_MASK ) | ( uint8_t )( ( uint16_t )( power - 5 ) & 0x0F );
+        }
+        else
+        {
+            if( power < 2 )
+            {
+                power = 2;
+            }
+            if( power > 17 )
+            {
+                power = 17;
+            }
+            paConfig = ( paConfig & RF_PACONFIG_OUTPUTPOWER_MASK ) | ( uint8_t )( ( uint16_t )( power - 2 ) & 0x0F );
+        }
+    }
+    else
+    {
+        if( power < -1 )
+        {
+            power = -1;
+        }
+        if( power > 14 )
+        {
+            power = 14;
+        }
+        paConfig = ( paConfig & RF_PACONFIG_OUTPUTPOWER_MASK ) | ( uint8_t )( ( uint16_t )( power + 1 ) & 0x0F );
+    }
+    SX1276Write( REG_PACONFIG, paConfig );
+    SX1276Write( REG_PADAC, paDac );
+}
+
 uint8_t SX1276GetPaSelect( uint32_t channel )
 {
 // TODO: I had to enable boost to get decent communication
