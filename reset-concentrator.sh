8e832af4f99c713df93e0a053ff7442fd67d5d04
Bruno Van de Velde <bruno@texus.me>
2017-03-25 16:03:58 +0000

    Don't let the reset script output an ignorable error which occurs when the script is ran multiple times

--- reset-concentrator.sh
+++ reset-concentrator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "25"  > /sys/class/gpio/export
+echo "25"  > /sys/class/gpio/export 2> /dev/null
 echo "out" > /sys/class/gpio/gpio25/direction
 echo "1"   > /sys/class/gpio/gpio25/value
 sleep 3
